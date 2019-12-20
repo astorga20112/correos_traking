33e1da8e59502bc93704fbab7c2985c11f8380d6
Fabián Villena <villenafabian@gmail.com>
2019-12-20 11:34:09 +0100

    adding referrer in correoschile query

--- api.php
+++ api.php
@@ -11,6 +11,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, $url);             
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_REFERER, 'https://www.correos.cl/web/guest/seguimiento-en-linea?codigos='.$code);
 
 $result = curl_exec($ch);
 
